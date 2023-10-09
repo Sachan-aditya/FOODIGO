48a96a0d94e5bef6433d574f417938c94b376bad
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-10-09 20:02:04 +0530

    AddedE

--- lib/screens/adminlogin.dart
+++ lib/screens/adminlogin.dart
@@ -1,63 +1,194 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:grocery/screens/login.dart';
-import 'package:lottie/lottie.dart';
 
-class Welcome extends StatefulWidget {
-  const Welcome({super.key});
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdminHomeScreen extends StatefulWidget {
+  const AdminHomeScreen({super.key});
 
   @override
-  State<Welcome> createState() => _WelcomeState();
+  State<AdminHomeScreen> createState() => _AdminHomeScreenState();
 }
 
-class _WelcomeState extends State<Welcome> {
+class _AdminHomeScreenState extends State<AdminHomeScreen> {
+  void logOut() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Log Out"),
+          content: const Text("Are you sure you want to log out?"),
+          actions: [
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text("Log Out"),
+              onPressed: () async {
+                Navigator.of(context).pop(); // Close the dialog
+                await FirebaseAuth.instance.signOut();
+                Navigator.popUntil(context, (route) => route.isFirst);
+                Navigator.pushReplacement(
+                  context,
+                  CupertinoPageRoute(builder: (context) => const AdminLogin()),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildGridButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+  }) {
+    final Size size = MediaQuery.of(context).size;
+    final double heightRatio = size.height / 915;
+    final double widthRatio = size.width / 412;
+    final double fontSize = 14.0 * heightRatio * widthRatio;
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.red[300],
+        backgroundColor: Colors.white, // Button text color
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        padding: const EdgeInsets.all(16.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 36.0 * MediaQuery.of(context).textScaleFactor,
+            color: Colors.red[300], // Icon color
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double heightRatio = size.height / 915;
+    final double widthRatio = size.width / 412;
+
     return Scaffold(
-      body: Column(
-        children: [
-          LottieBuilder.asset('assets/animation_lndhvqe8.json'),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.04,
+      appBar: AppBar(
+        backgroundColor: Colors.red[300],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.white), // Change icon color
+        ),
+        title: Text(
+          "Manager",
+          style: TextStyle(
+            fontSize: 26 * widthRatio * heightRatio,
+            color: Colors.white, // Change title text color
           ),
-          Text("FOODIGO",
-              style: GoogleFonts.lato(
-                  textStyle: const  TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.brown))),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Made with  ",
-                style: GoogleFonts.lato(
-                    textStyle: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.black)),
-              ),
-              FaIcon(
-                FontAwesomeIcons.heart,
-                color: Colors.red,
-              ),
-              Text("  by ADITYA SACHAN ",
-                  style: GoogleFonts.lato(
-                      textStyle: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black)))
-            ],
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: logOut,
+            icon: Icon(
+              Icons.logout,
+              size: 32 * widthRatio * heightRatio,
+              color: Colors.white, // Change icon color
+            ),
           ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => Loginscreen()));
-              },
-              child: Text("PRESS"))
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 16.0, // Add spacing between buttons
+          crossAxisSpacing: 16.0, // Add spacing between buttons
+          children: [
+            buildGridButton(
+              icon: Icons.shopping_cart,
+              label: "Sales Form",
+              onPressed: () {
+                // Add your action for button 1
+              },
+            ),
+            buildGridButton(
+              icon: Icons.access_time,
+              label: "Today's Work",
+              onPressed: () {
+                // Add your action for button 2
+              },
+            ),
+            buildGridButton(
+              icon: Icons.location_on,
+              label: "Tracking",
+              onPressed: () {
+                // Add your action for button 3
+              },
+            ),
+            buildGridButton(
+              icon: Icons.pie_chart,
+              label: "Sales Data",
+              onPressed: () {
+                // Add your action for button 4
+              },
+            ),
+            buildGridButton(
+              icon: Icons.monetization_on,
+              label: "Earnings",
+              onPressed: () {
+                // Add your action for button 5
+              },
+            ),
+            buildGridButton(
+              icon: Icons.people,
+              label: "Employee Performance",
+              onPressed: () {
+                // Add your action for button 6
+              },
+            ),
+            buildGridButton(
+              icon: Icons.person_add,
+              label: "Employee Registration",
+              onPressed: () {
+                // Add your action for button 7
+              },
+            ),
+            buildGridButton(
+              icon: Icons.delete_forever,
+              label: "Delete Customer",
+              onPressed: () {
+                // Add your action for button 8
+              },
+            ),
+            buildGridButton(
+              icon: Icons.person_remove,
+              label: "Delete Employee",
+              onPressed: () {
+                // Add your action for button 9
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
